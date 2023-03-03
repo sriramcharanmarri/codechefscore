acf4d526199ed2ed48011289d9ffd7167b3cdd62
20r21a6228 <93866056+20r21a6228@users.noreply.github.com>
2023-03-04 00:26:55 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
 include("simple_html_dom.php");
 
 $html = file_get_html('https://www.codechef.com/users/jaswanth_111');
-
+echo "jaswanth ne codechef score \n";
 echo $html->find('.rating-number',0)->innertext;
+echo "  jaswanth ne codechef stars  ";
 echo $html->find('.rating-star',0)->innertext;
 ?>
